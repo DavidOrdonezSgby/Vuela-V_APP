a768c4dda0772dbec1ae17eb09f355f6de4bbc5e
DavidOrdonezSgby <david.ordonez.est@tecazuay.edu.ec>
2022-06-03 20:43:37 -0500

    Nuevo

--- app/src/main/java/com/example/vuelav_app/Registro.java
+++ app/src/main/java/com/example/vuelav_app/Registro.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Registro extends AppCompatActivity {
 //Interfaz registro
+    //asd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
