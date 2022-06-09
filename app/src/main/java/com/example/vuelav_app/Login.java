400c68e45c3cf8ddf95ad68306a678b33b826fe0
DavidOrdonezSgby <david.ordonez.est@tecazuay.edu.ec>
2022-06-08 20:05:40 -0500

    Login

--- app/src/main/java/com/example/vuelav_app/Login.java
+++ app/src/main/java/com/example/vuelav_app/Login.java
@@ -6,10 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.vuelav_app.Logico.Apis;
+import com.example.vuelav_app.Logico.Models.Usuario;
+import com.example.vuelav_app.Logico.Service.UsuarioService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
-    private Button Registro;
+    private Button Registro, Iniciar;
+    private UsuarioService usuarioService = Apis.getUsuarioService();
+    private EditText a1, a2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,39 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         Registro = (Button) findViewById(R.id.btnRegistrarse);
         Registro.setOnClickListener(this);
+
+        Iniciar = (Button) findViewById(R.id.btnIniciarSesion);
+        Iniciar.setOnClickListener(this);
+
+        a1 = (EditText) findViewById(R.id.txtUsername);
+        a2 = (EditText) findViewById(R.id.as);
+    }
+
+    public void revisarcorreo(Usuario usuario){
+
+        Call<Usuario> call=usuarioService.login(usuario);
+        call.enqueue(new Callback<Usuario>() {
+            @Override
+            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                if(response.isSuccessful()){
+                    System.out.println("entro y si valio");
+                    if(response.body().getEmail().equals(a1.getText().toString())){
+                        System.out.println("Coincide");
+                    }else{
+                        System.out.println("No coincide");
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Usuario> call, Throwable t) {
+                System.out.println("fallooooooooo");
+                System.out.println(t.toString());
+
+            }
+        });
+
     }
 
     private void goToRegistro() {
@@ -32,7 +77,12 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             case R.id.btnRegistrarse:
                 goToRegistro();
                 break;
+            case R.id.btnIniciarSesion:
+                Usuario usuario=new Usuario(a1.getText().toString(),a2.getText().toString());
+                revisarcorreo(usuario);
+                break;
 
         }
     }
+
 }
