4abfbb21f4160154f60eb7adb26eb125fbfc9e55
DavidOrdonezSgby <david.ordonez.est@tecazuay.edu.ec>
2022-06-16 01:34:35 -0500

    Modificacion Fecha Nacimiento

--- app/src/main/java/com/example/vuelav_app/Registro.java
+++ app/src/main/java/com/example/vuelav_app/Registro.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.vuelav_app.Logico.Apis;
 import com.example.vuelav_app.Logico.Models.RegisterRequest;
@@ -30,16 +33,18 @@ public class Registro extends AppCompatActivity {
     private UsuarioService usuarioService = Apis.getUsuarioService();
 
     private EditText nombre, apellido, cedula ,razonsocial, telefono, email, contra;
-    private DatePickerDialog btnDate;
-    private Button btnDates;
     private Date date1;
     private String fecha2;
+    private TextView visualizaFecha;
+    private Button btnDates;
+    private DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         btnDates = (Button) findViewById(R.id.btnDate);
+        visualizaFecha = (TextView) findViewById(R.id.txtVisualizaFecha);
         nombre = (EditText) findViewById(R.id.txtNombreRegistro);
         apellido = (EditText) findViewById(R.id.txtApellidoRegistro);
        cedula = (EditText) findViewById(R.id.txtCedulaRegistro);
@@ -47,46 +52,31 @@ public class Registro extends AppCompatActivity {
         razonsocial = (EditText)findViewById(R.id.txtRazonSocialRegistro);
         email = (EditText) findViewById(R.id.txtUsernameRegistro);
         contra = (EditText) findViewById(R.id.txtPasswordregistro);
-        findViewById(R.id.btnDate).setOnClickListener(l -> abrirfecha());
 
-    }
-    private void fechaelegida() {
-       DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        btnDates.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSet(DatePicker datePicker, int anio, int mes, int dia) {
-                mes = mes + 1;
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        Registro.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        ,setListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
 
-                 String fecha = anio + "-" + mes + "-" + dia;
-
-                try {
-                    SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
-                    fecha2 = formato.format(fecha);
-                    date1 = formato.parse(fecha2) ;
-                    System.out.println(date1);
-                    System.out.println(fecha);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-
-                btnDates.setText(anio + "    -   " + mes + "     -   " + dia);
-
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayofMoth) {
+                month = month+1;
+                String date = year+"/"+month+"/"+day;
+                btnDates.setText(date);
             }
         };
-        Calendar cal = Calendar.getInstance();
-        int anio = cal.get(Calendar.YEAR);
-        int mes = cal.get(Calendar.MONTH);
-        int dia = cal.get(Calendar.DAY_OF_MONTH);
-        int estilo = AlertDialog.BUTTON_POSITIVE;
-
-        btnDate = new DatePickerDialog(this, estilo, date, anio, mes, dia);
-        btnDate.getDatePicker().setMaxDate(System.currentTimeMillis());
-    }
-
-
-    private void abrirfecha() {
-        fechaelegida();
-        btnDate.show();
     }
 
     public void crearusuario(View view){
@@ -97,6 +87,7 @@ public class Registro extends AppCompatActivity {
         usuario.setApellidos(apellido.getText().toString());
         usuario.setRazonSocial(razonsocial.getText().toString());
         usuario.setFechaNacimiento(date1);
+        System.out.println("FECHA REGISTRO"+ date1);
         usuario.setTelefono(telefono.getText().toString());
         usuario.setEmail(email.getText().toString());
         usuario.setClave(contra.getText().toString());
