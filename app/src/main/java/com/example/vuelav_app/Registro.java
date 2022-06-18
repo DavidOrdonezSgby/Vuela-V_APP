02ba32626f0a23aceeb370b645968693b11691d7
DavidOrdonezSgby <david.ordonez.est@tecazuay.edu.ec>
2022-06-17 21:09:41 -0500

    Registro Acepto Términos

--- app/src/main/java/com/example/vuelav_app/Registro.java
+++ app/src/main/java/com/example/vuelav_app/Registro.java
@@ -10,14 +10,10 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.vuelav_app.Logico.Apis;
@@ -34,17 +30,15 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class Registro extends AppCompatActivity {
+public class Registro extends AppCompatActivity implements View.OnClickListener{
 
 
     private UsuarioService usuarioService = Apis.getUsuarioService();
 
     private EditText nombre, apellido, cedula ,razonsocial, telefono, email, contra;
-    private Button btnDates;
+    private Button btnDates, completarRegistro;
     private DatePickerDialog.OnDateSetListener setListener;
-    private String fecha, sexo;
-    private Spinner sppais;
-    private RadioButton rdMasculino,rdFemenino;
+    private String fecha;
     private CheckBox boxAceptarterminos;
     private boolean terminos;
 
@@ -60,16 +54,10 @@ public class Registro extends AppCompatActivity {
         razonsocial = (EditText)findViewById(R.id.txtRazonSocialRegistro);
         email = (EditText) findViewById(R.id.txtUsernameRegistro);
         contra = (EditText) findViewById(R.id.txtPasswordregistro);
-        sppais = findViewById(R.id.spPaisRegistro);
-        rdMasculino = findViewById(R.id.rdregistromasculino);
-        rdFemenino = findViewById(R.id.rdregistrofemenino);
         boxAceptarterminos = findViewById(R.id.boxterminoscondiciones);
-        CargarPaises();
-        IniciarFecha();
+        completarRegistro = (Button) findViewById(R.id.btnRegistrar);
+        completarRegistro.setOnClickListener(this);
 
-    }
-
-    private void IniciarFecha(){
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
@@ -103,51 +91,18 @@ public class Registro extends AppCompatActivity {
             }
         };
     }
-    private void TipoSexo(){
-        if(rdMasculino.isChecked()==true){
-            sexo="Masculino";
-        }else{
-            if(rdFemenino.isChecked()==true){
-                sexo="Femenino";
-            }
-        }
-        System.out.println(sexo);
-    }
 
-    private boolean AceptacionTerminos(){
-        if(boxAceptarterminos.isChecked()==true){
-            terminos = true;
-            return true;
-        }
-        terminos = false;
-        return false;
-    }
-
-
-    private void DespuesTerminos(){
-        if(AceptacionTerminos()==true){
-            showRegisterComplete();
-        }else{
-            Toast.makeText(Registro.this,"Debe Aceptar los Términos y Condiciones",Toast.LENGTH_LONG).show();
-        }
-    }
-    public void crearusuario(View view){
+    public void crearusuario(){
         System.out.println( ":v entro");
         RegisterRequest usuario = new RegisterRequest();
         usuario.setDocIdentificacion(cedula.getText().toString());
         usuario.setNombres(nombre.getText().toString());
         usuario.setApellidos(apellido.getText().toString());
         usuario.setRazonSocial(razonsocial.getText().toString());
-        usuario.setPais(sppais.getSelectedItem().toString());
         usuario.setFechaNacimiento(fecha);
         usuario.setTelefono(telefono.getText().toString());
         usuario.setEmail(email.getText().toString());
         usuario.setClave(contra.getText().toString());
-        TipoSexo();
-        usuario.setGenero(sexo);
-        AceptacionTerminos();
-        usuario.setTerminosCondiciones(terminos);
-
 
         Call<UsuarioResponse> call=usuarioService.signup(usuario);
         System.out.println("paso los datosd");
@@ -155,9 +110,9 @@ public class Registro extends AppCompatActivity {
             @Override
             public void onResponse(Call<UsuarioResponse> call, Response<UsuarioResponse> response) {
                 if(response.isSuccessful()){
-                    DespuesTerminos();
                     System.out.println("entro y si valio \n Creadouski");
-
+                    DespuesTerminos();
+                    showRegisterComplete();
                 }else{
                     System.out.println("Esta cedula ya esta creada");
                     showRegisterError();
@@ -184,7 +139,7 @@ public class Registro extends AppCompatActivity {
         btnComplete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SesionIniciada.class);
+                Intent intent = new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
             }
         });
@@ -208,34 +163,77 @@ public class Registro extends AppCompatActivity {
         dialog.show();
     }
 
-    private void CargarPaises(){
-        String[] profesion = {"Seleccione el país de origen...", "Afganistán","Albania","Alemania","Andorra",
-                "Angola","Antigua y Barbuda","Arabia Saudita","Argelia","Argentina","Armenia","Australia",
-                "Austria","Azerbaiyán","Bahamas","Bangladés","Barbados","Baréin","Bélgica","Belice","Benín",
-                "Bielorrusia","Birmania","Bolivia","Bosnia y Herzegovina","Botsuana","Brasil","Brunéi",
-                "Bulgaria","Burkina Faso","Burundi","Bután","Cabo Verde","Camboya","Camerún","Canadá","Catar",
-                "Chad","Chile","China","Chipre","Ciudad del Vaticano","Colombia","Comoras","Corea del Norte",
-                "Corea del Sur","Costa de Marfil","Costa Rica","Croacia","Cuba","Dinamarca","Dominica","Ecuador",
-                "Egipto","El Salvador","Emiratos Árabes Unidos","Eritrea","Eslovaquia","Eslovenia","España",
-                "Estados Unidos","Estonia","Etiopía","Filipinas","Finlandia","Fiyi","Francia","Gabón","Gambia",
-                "Georgia","Ghana","Granada","Grecia","Guatemala","Guyana","Guinea","Guinea ecuatorial","Guinea-Bisáu",
-                "Haití","Honduras","Hungría","India","Indonesia","Irak","Irán","Irlanda","Islandia","Islas Marshall",
-                "Islas Salomón","Israel","Italia","Jamaica","Japón","Jordania","Kazajistán","Kenia","Kirguistán",
-                "Kiribati","Kuwait","Laos","Lesoto","Letonia","Líbano","Liberia","Libia","Liechtenstein","Lituania",
-                "Luxemburgo","Madagascar","Malasia","Malaui","Maldivas","Malí","Malta","Marruecos","Mauricio",
-                "Mauritania","México","Micronesia","Moldavia","Mónaco","Mongolia","Montenegro","Mozambique","Namibia",
-                "Nauru","Nepal","Nicaragua","Níger","Nigeria","Noruega","Nueva Zelanda","Omán","Países Bajos","Pakistán",
-                "Palaos","Palestina","Panamá","Papúa Nueva Guinea","Paraguay","Perú","Polonia","Portugal","Reino Unido",
-                "República Centroafricana","República Checa","República de Macedonia","República del Congo",
-                "República Democrática del Congo","República Dominicana","República Sudafricana","Ruanda","Rumanía",
-                "Rusia","Samoa","San Cristóbal y Nieves","San Marino","San Vicente y las Granadinas","Santa Lucía",
-                "Santo Tomé y Príncipe","Senegal","Serbia","Seychelles","Sierra Leona","Singapur","Siria","Somalia","Sri Lanka",
-                "Suazilandia","Sudán","Sudán del Sur","Suecia","Suiza","Surinam","Tailandia","Tanzania","Tayikistán","Timor Oriental",
-                "Togo","Tonga","Trinidad y Tobago","Túnez","Turkmenistán","Turquía","Tuvalu","Ucrania","Uganda","Uruguay","Uzbekistán",
-                "Vanuatu","Venezuela","Vietnam","Yemen","Yibuti","Zambia","Zimbabue", "OTRO..."};
+    private boolean AceptacionTerminos(){
+        if(boxAceptarterminos.isChecked()==true){
+            terminos = true;
+            return true;
+        }
+        terminos = false;
+        return false;
+    }
 
-        ArrayAdapter<String> pais = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, profesion);
-        sppais.setAdapter(pais);
 
+    private void DespuesTerminos(){
+        if(AceptacionTerminos()==true){
+            showRegisterComplete();
+        }else{
+            Toast.makeText(Registro.this,"Debe Aceptar los Términos y Condiciones",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean validar() {
+        boolean retorno = true;
+        String c1 = cedula.getText().toString();
+        String c2 = nombre.getText().toString();
+        String c3 = apellido.getText().toString();
+        String c4 = razonsocial.getText().toString();
+        String c5 = telefono.getText().toString();
+        String c6 = email.getText().toString();
+        String c7 = contra.getText().toString();
+
+        if (c1.isEmpty()) {
+            cedula.setError("Ingresar la cédula");
+            retorno = false;
+        }
+        if (c2.isEmpty()) {
+            nombre.setError("Ingresar el nombre");
+            retorno = false;
+        }
+        if (c3.isEmpty()) {
+            apellido.setError("Ingresar el apellido");
+            retorno = false;
+        }
+        if (c4.isEmpty()) {
+            razonsocial.setError("Ingresar la razón social");
+            retorno = false;
+        }
+        if (c5.isEmpty()) {
+            telefono.setError("Ingresar el teléfono");
+            retorno = false;
+        }
+        if (c6.isEmpty()) {
+            email.setError("Ingresar el email");
+            retorno = false;
+        }
+        if (c7.isEmpty()) {
+            contra.setError("Ingresar una contraseña");
+            retorno = false;
+        }
+
+        return retorno;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btnRegistrar:
+                if(!validar()){
+                    Toast.makeText(this, "Ingresar Datos", Toast.LENGTH_SHORT).show();
+                }else{
+                    crearusuario();
+                }
+                break;
+        }
     }
 }
