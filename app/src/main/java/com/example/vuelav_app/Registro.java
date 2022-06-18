84f70ea7c18e239614dcc1e54aafc674f12a7d25
elvis-git <elvis.vele.est@tecazuay.edu.ec>
2022-06-18 01:01:17 -0500

    Notificacion - Registro

--- app/src/main/java/com/example/vuelav_app/Registro.java
+++ app/src/main/java/com/example/vuelav_app/Registro.java
@@ -1,19 +1,31 @@
 package com.example.vuelav_app;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.vuelav_app.Logico.Apis;
@@ -38,61 +50,83 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
     private EditText nombre, apellido, cedula ,razonsocial, telefono, email, contra;
     private Button btnDates, completarRegistro;
     private DatePickerDialog.OnDateSetListener setListener;
-    private String fecha;
+    private String fecha, sexo;
+    private Spinner sppais;
+    private RadioButton rdMasculino,rdFemenino;
     private CheckBox boxAceptarterminos;
     private boolean terminos;
+    private PendingIntent pendingIntent;
+    private final static  String CHANNEL_ID="NOFTIFICACION";
+    private final static int NOTIFICACION_ID =0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-        btnDates = (Button) findViewById(R.id.btnDate);
-        nombre = (EditText) findViewById(R.id.txtNombreRegistro);
-        apellido = (EditText) findViewById(R.id.txtApellidoRegistro);
-       cedula = (EditText) findViewById(R.id.txtCedulaRegistro);
-        telefono = (EditText) findViewById(R.id.txtCelularRegistro);
-        razonsocial = (EditText)findViewById(R.id.txtRazonSocialRegistro);
-        email = (EditText) findViewById(R.id.txtUsernameRegistro);
-        contra = (EditText) findViewById(R.id.txtPasswordregistro);
-        boxAceptarterminos = findViewById(R.id.boxterminoscondiciones);
+        dtsfindVew();
         completarRegistro = (Button) findViewById(R.id.btnRegistrar);
         completarRegistro.setOnClickListener(this);
+        CargarPaises();
+        MostrarFecha();
+        setPendingIntent();
 
-        Calendar calendar = Calendar.getInstance();
-        final int year = calendar.get(Calendar.YEAR);
-        final int month = calendar.get(Calendar.MONTH);
-        final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        btnDates.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(
-                        Registro.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
-                        ,setListener, year, month, day);
-                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                datePickerDialog.show();
-            }
-        });
 
-        setListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayofMoth) {
-                Calendar mCalendar = Calendar.getInstance();
-                mCalendar.set(Calendar.YEAR, year);
-                mCalendar.set(Calendar.MONTH, month);
-                mCalendar.set(Calendar.DAY_OF_MONTH, dayofMoth);
-                String selectDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
-                btnDates.setText(selectDate);
-                System.out.println(selectDate);
-
-                SimpleDateFormat formats = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-                fecha = formats.format(mCalendar.getTime());
-
-            }
-        };
     }
 
-    public void crearusuario(){
+private void dtsfindVew(){
+    btnDates = (Button) findViewById(R.id.btnDate);
+    nombre = (EditText) findViewById(R.id.txtNombreRegistro);
+    apellido = (EditText) findViewById(R.id.txtApellidoRegistro);
+    cedula = (EditText) findViewById(R.id.txtCedulaRegistro);
+    telefono = (EditText) findViewById(R.id.txtCelularRegistro);
+    razonsocial = (EditText)findViewById(R.id.txtRazonSocialRegistro);
+    email = (EditText) findViewById(R.id.txtUsernameRegistro);
+    contra = (EditText) findViewById(R.id.txtPasswordregistro);
+    boxAceptarterminos = findViewById(R.id.boxterminoscondiciones);
+    sppais = findViewById(R.id.spPaisRegistro);
+    rdMasculino = findViewById(R.id.rdregistromasculino);
+    rdFemenino = findViewById(R.id.rdregistrofemenino);
+    boxAceptarterminos = findViewById(R.id.boxterminoscondiciones);
+
+}
+private void MostrarFecha(){
+    Calendar calendar = Calendar.getInstance();
+    final int year = calendar.get(Calendar.YEAR);
+    final int month = calendar.get(Calendar.MONTH);
+    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+    btnDates.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    Registro.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                    ,setListener, year, month, day);
+            datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            datePickerDialog.show();
+        }
+    });
+
+    setListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayofMoth) {
+            Calendar mCalendar = Calendar.getInstance();
+            mCalendar.set(Calendar.YEAR, year);
+            mCalendar.set(Calendar.MONTH, month);
+            mCalendar.set(Calendar.DAY_OF_MONTH, dayofMoth);
+            String selectDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
+            btnDates.setText(selectDate);
+            System.out.println(selectDate);
+
+            SimpleDateFormat formats = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            fecha = formats.format(mCalendar.getTime());
+
+        }
+    };
+}
+
+    private void crearusuario(){
         System.out.println( ":v entro");
         RegisterRequest usuario = new RegisterRequest();
         usuario.setDocIdentificacion(cedula.getText().toString());
@@ -103,15 +137,22 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
         usuario.setTelefono(telefono.getText().toString());
         usuario.setEmail(email.getText().toString());
         usuario.setClave(contra.getText().toString());
+        usuario.setPais(sppais.getSelectedItem().toString());
+        TipoSexo();
+        usuario.setGenero(sexo);
+        usuario.setTerminosCondiciones(terminos);
 
         Call<UsuarioResponse> call=usuarioService.signup(usuario);
         System.out.println("paso los datosd");
         call.enqueue(new Callback<UsuarioResponse>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onResponse(Call<UsuarioResponse> call, Response<UsuarioResponse> response) {
                 if(response.isSuccessful()){
-                    System.out.println("entro y si valio \n Creadouski");
                     DespuesTerminos();
+
+                    System.out.println("entro y si valio \n Creadouski");
+
                 }else{
                     System.out.println("Esta cedula ya esta creada");
                     showRegisterError();
@@ -125,6 +166,58 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
 
             }
         });
+    }
+    private void setPendingIntent(){
+        Intent intent = new Intent(this, SesionIniciada.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(SesionIniciada.class);
+        stackBuilder.addNextIntent(intent);
+        pendingIntent = stackBuilder.getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+    private void EviarNotificacion() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_baseline_how_to_reg_24);
+        builder.setContentTitle("Notificacion - Registro con éxito");
+        builder.setColor(Color.BLUE);
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setLights(Color.MAGENTA,1000,1000);
+        builder.setVibrate(new long[]{1000,1000,1000});
+        builder.setDefaults(Notification.DEFAULT_SOUND);
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManagerCompat= NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICACION_ID,builder.build());
+    }
+
+    private void CargarPaises(){
+        String[] paises = {"Seleccione el país de origen...", "Afganistán","Albania","Alemania","Andorra",
+                "Angola","Antigua y Barbuda","Arabia Saudita","Argelia","Argentina","Armenia","Australia",
+                "Austria","Azerbaiyán","Bahamas","Bangladés","Barbados","Baréin","Bélgica","Belice","Benín",
+                "Bielorrusia","Birmania","Bolivia","Bosnia y Herzegovina","Botsuana","Brasil","Brunéi",
+                "Bulgaria","Burkina Faso","Burundi","Bután","Cabo Verde","Camboya","Camerún","Canadá","Catar",
+                "Chad","Chile","China","Chipre","Ciudad del Vaticano","Colombia","Comoras","Corea del Norte",
+                "Corea del Sur","Costa de Marfil","Costa Rica","Croacia","Cuba","Dinamarca","Dominica","Ecuador",
+                "Egipto","El Salvador","Emiratos Árabes Unidos","Eritrea","Eslovaquia","Eslovenia","España",
+                "Estados Unidos","Estonia","Etiopía","Filipinas","Finlandia","Fiyi","Francia","Gabón","Gambia",
+                "Georgia","Ghana","Granada","Grecia","Guatemala","Guyana","Guinea","Guinea ecuatorial","Guinea-Bisáu",
+                "Haití","Honduras","Hungría","India","Indonesia","Irak","Irán","Irlanda","Islandia","Islas Marshall",
+                "Islas Salomón","Israel","Italia","Jamaica","Japón","Jordania","Kazajistán","Kenia","Kirguistán",
+                "Kiribati","Kuwait","Laos","Lesoto","Letonia","Líbano","Liberia","Libia","Liechtenstein","Lituania",
+                "Luxemburgo","Madagascar","Malasia","Malaui","Maldivas","Malí","Malta","Marruecos","Mauricio",
+                "Mauritania","México","Micronesia","Moldavia","Mónaco","Mongolia","Montenegro","Mozambique","Namibia",
+                "Nauru","Nepal","Nicaragua","Níger","Nigeria","Noruega","Nueva Zelanda","Omán","Países Bajos","Pakistán",
+                "Palaos","Palestina","Panamá","Papúa Nueva Guinea","Paraguay","Perú","Polonia","Portugal","Reino Unido",
+                "República Centroafricana","República Checa","República de Macedonia","República del Congo",
+                "República Democrática del Congo","República Dominicana","República Sudafricana","Ruanda","Rumanía",
+                "Rusia","Samoa","San Cristóbal y Nieves","San Marino","San Vicente y las Granadinas","Santa Lucía",
+                "Santo Tomé y Príncipe","Senegal","Serbia","Seychelles","Sierra Leona","Singapur","Siria","Somalia","Sri Lanka",
+                "Suazilandia","Sudán","Sudán del Sur","Suecia","Suiza","Surinam","Tailandia","Tanzania","Tayikistán","Timor Oriental",
+                "Togo","Tonga","Trinidad y Tobago","Túnez","Turkmenistán","Turquía","Tuvalu","Ucrania","Uganda","Uruguay","Uzbekistán",
+                "Vanuatu","Venezuela","Vietnam","Yemen","Yibuti","Zambia","Zimbabue", "OTRO..."};
+
+        ArrayAdapter<String> pais = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, paises);
+        sppais.setAdapter(pais);
+
     }
 
     private void showRegisterComplete(){
@@ -164,25 +257,37 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
         dialog.show();
     }
 
-    private boolean AceptacionTerminos(){
-        if(boxAceptarterminos.isChecked()==true){
-            terminos = true;
-            return true;
-        }
-        terminos = false;
-        return false;
-    }
-
-
-    private void DespuesTerminos(){
-        if(AceptacionTerminos()==true){
-            showRegisterComplete();
+    private void TipoSexo(){
+        if(rdMasculino.isChecked()==true){
+            sexo="Masculino";
         }else{
-            Toast.makeText(Registro.this,"Debe Aceptar los Términos y Condiciones",Toast.LENGTH_LONG).show();
+            if(rdFemenino.isChecked()==true){
+                sexo="Femenino";
+            }
+        }
+        System.out.println(sexo);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void DespuesTerminos(){
+        if(terminos==true){
+            showRegisterComplete();
+            EviarNotificacion();
+            createNotificationChanne();
         }
     }
 
-    public boolean validar() {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotificationChanne() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ){
+            CharSequence name = "Notificacion - Registro Exitoso";
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,name,NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private boolean validar() {
         boolean retorno = true;
         String c1 = cedula.getText().toString();
         String c2 = nombre.getText().toString();
@@ -219,6 +324,18 @@ public class Registro extends AppCompatActivity implements View.OnClickListener{
         if (c7.isEmpty()) {
             contra.setError("Ingresar una contraseña");
             retorno = false;
+        }
+        if(sppais.getSelectedItem().toString().equals("Seleccione el país de origen...")){
+            System.out.println("Seleccione el pais" );
+            Toast.makeText(Registro.this,"Seleccione su pais de origen",Toast.LENGTH_LONG).show();
+    retorno = false;
+        }
+        if(boxAceptarterminos.isChecked()==false){
+            Toast.makeText(Registro.this,"Debe Aceptar los Términos y Condiciones",Toast.LENGTH_LONG).show();
+            terminos = false;
+            retorno = false;
+        }else{
+            terminos = true;
         }
 
         return retorno;
