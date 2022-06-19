59c08a10b7163afaaa373240268943c920770642
Edi-JV <edigonzalez06@gmail.com>
2022-06-19 13:41:37 -0500

    Creacion Servicios Recerva

--- app/src/main/java/com/example/vuelav_app/Logico/Service/ReservaService.java
+++ app/src/main/java/com/example/vuelav_app/Logico/Service/ReservaService.java
@@ -1,5 +1,21 @@
 package com.example.vuelav_app.Logico.Service;
 
+import com.example.vuelav_app.Logico.Request.ReservaRequest;
+import com.example.vuelav_app.Logico.Response.ReservaResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 public interface ReservaService {
+
+    @POST("/api/reserva")
+    Call<?> Create(ReservaRequest reservaRequest);
+
+    @GET("/api/reserva/reservasByUsuario/{id}")
+    Call<List<ReservaResponse>> obtReservacion(@Path("id") int id);
 
 }
