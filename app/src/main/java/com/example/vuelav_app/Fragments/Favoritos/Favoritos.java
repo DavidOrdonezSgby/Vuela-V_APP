609073769811503ac515695a8213202e16fadf38
DavidOrdonezSgby <david.ordonez.est@tecazuay.edu.ec>
2022-06-27 17:43:59 -0500

    Favoritos 2

--- app/src/main/java/com/example/vuelav_app/Fragments/Favoritos/Favoritos.java
+++ app/src/main/java/com/example/vuelav_app/Fragments/Favoritos/Favoritos.java
@@ -3,14 +3,29 @@ package com.example.vuelav_app.Fragments.Favoritos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.vuelav_app.Fragments.NotificationFragmentModel.NotificacionesAdaptador;
+import com.example.vuelav_app.Logico.Response.ReservaResponse;
 import com.example.vuelav_app.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Favoritos extends AppCompatActivity {
+
+    //ArrayList<String> strings = new ArrayList<>();
+
+    //List<String> rv = new ArrayList<String>();
+    private ListView listviewFavoritos = null;
+    FavoritosAdapter adaptador = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoritos);
+
+        //adaptador = new NotificacionesAdaptador(getApplicationContext(), R.layout.item_favoritos, rv);
+        listviewFavoritos = findViewById(R.id.favoritos_list);
     }
 }
--- app/src/main/java/com/example/vuelav_app/Fragments/Favoritos/FavoritosAdapter.java
+++ app/src/main/java/com/example/vuelav_app/Fragments/Favoritos/FavoritosAdapter.java
@@ -1,6 +1,46 @@
 package com.example.vuelav_app.Fragments.Favoritos;
 
-public class FavoritosAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.vuelav_app.Logico.Response.ReservaResponse;
+import com.example.vuelav_app.R;
+
+import java.util.List;
+
+
+public class FavoritosAdapter extends ArrayAdapter<String> {
+
+    private Context context;
+    private List<String> favorito;
+
+    public FavoritosAdapter(@NonNull Context context, int resource, List<String> favorito) {
+        super(context, resource, favorito);
+        this.context = context;
+        this.favorito = favorito;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.item_favoritos, parent, false);
+        TextView Nomlugar = (TextView) row.findViewById(R.id.txtNombreLugarF);
+        TextView idVuelo = (TextView) row.findViewById(R.id.txtvueloid);
+        TextView ciudad = (TextView) row.findViewById(R.id.txtCiudadF);
+        TextView precio = (TextView) row.findViewById(R.id.txtPrecioF);
+
+
+
+        return row;
+    }
 
 }
